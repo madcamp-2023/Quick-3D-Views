bb5265f2b79ea465b3f521c8992b7901a7797a31
haejunejung <jhj9422@unist.ac.kr>
2024-01-20 00:13:13 +0900

    [#6] feat : snow animation && merge

--- src/lib/index.tsx
+++ src/lib/index.tsx
@@ -1,3 +1,5 @@
 export { default as RippleAnimation } from "./RippleAnimation";
 export { default as FlowerAnimation } from "./FlowerAnimation";
 export { default as FallenLeavesAnimation } from "./FallenLeavesAnimation";
+export { default as SnowAnimation } from "./SnowAnimation";
+export { default as RainAnimation } from "./RainAnimation";
